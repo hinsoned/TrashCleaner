d2a6e71b04f06766d2fbaecf392399ae888dfb76
hinsoned <102074110+hinsoned@users.noreply.github.com>
2025-09-02 13:22:59 -0400

    Readability and comments
    Improved readability and comments

--- MoveImagesToTrash.java
+++ MoveImagesToTrash.java
@@ -5,14 +5,21 @@ import java.nio.file.Files;
 import java.awt.Desktop;
 
 public class MoveImagesToTrash {
-    public static void main(String[] args) throws Exception{
-        //Get the pat to the desktop folder saved in to a Path object
+    public static void main(String[] args) throws Exception {
+        // Get the path to the desktop folder saved in to a Path object
         Path desktopPath = Paths.get(System.getProperty("user.home"), "Desktop");
 
         Stream<Path> files = Files.list(desktopPath);// Creates a stream of paths to the files in the desktop folder
         files.filter(path -> Files.isRegularFile(path))// Filters the stream to only include regular files
-        .filter(path -> path.getFileName().toString().endsWith(".jpg") || path.getFileName().toString().endsWith(".png"))// Filters the stream to only include files with the extensions .jpg or .png
-        .forEach(path -> Desktop.getDesktop().moveToTrash(path.toFile()));// Moves the files to the trash. Desktop requires the awt.desktop package
+                .filter(path -> path.getFileName().toString().toLowerCase().endsWith(".jpg") ||
+                        path.getFileName().toString().toLowerCase().endsWith(".png"))// Filters the stream to only
+                                                                                     // include files with the
+                                                                                     // extensions .jpg or .png
+                .forEach(path -> {
+                    System.out.println("Moving " + path.getFileName() + " to trash");
+                    Desktop.getDesktop().moveToTrash(path.toFile());// Moves the files to the trash. Desktop requires
+                                                                    // the awt.desktop package
+                });
 
         files.close();// Closes the stream
     }
